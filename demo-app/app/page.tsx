e512b06bdf9310290452d9840a59b49fad4ebd2d
Vikas <xanderbilla@gmail.com>
2025-05-05 02:56:00 +0530

    try 1# to build on vercel

--- demo-app/app/page.tsx
+++ demo-app/app/page.tsx
@@ -37,7 +37,7 @@ const WorkProcess = dynamic(
 );
 
 const Contact = dynamic(
-  () => import("@/components/Contact/Contact"),
+  () => import("../components/Contact/Contact"),
   {
     loading: () => <ContactSkeleton />,
   }
